2157d0561750a3552546e0b140c79c375a3543b1
Hildana-ui <hildana.amare@bitscollege.edu.et>
2026-05-30 23:18:29 +0300

    Complete task 6 business rules validations

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,3 +1,33 @@
 class Equipment < ApplicationRecord
-  belongs_to :category
+  belongs_to :category 
+  has_many :maintenance_records, dependent: :destroy
+
+  ALLOWED_STATUSES =%w[available in_use maintenance].freeze
+  
+  validates :name, presence: true
+  validates :status, presence: true, inclusion: { in: ALLOWED_STATUSES }
+  validates :serial_number, presence: true, uniqueness: true
+  validates :category_id, presence: true
+
+  validate :serial_number_format_must_be_valid
+  validate : name_must_be_real
+
+  private
+
+  def serial_number_format_must_be_valid
+    return if serial_number.blank?
+
+    unless serial_number.match?(/\A[A-Z]{3}-\d{3}\z/)
+      errors.add(:serial_number, "must match format XXX-NNN (three uppercase letters, a dash , three digits)")
+    end
+
+  end
+
+  def name_must_be_real
+    return if name.blank?
+
+    unless name.match?(/[A-Za-z]/)
+      errors.add(:name, "must contain at least one letter")
+    end
+  end
 end
--- app/models/maintenance_record.rb
+++ app/models/maintenance_record.rb
@@ -1,3 +1,19 @@
 class MaintenanceRecord < ApplicationRecord
   belongs_to :equipment
+
+  validates :description, presence: true
+  validates :performed_at, presence: true
+  validates :equipment_id, presence:true
+  
+  validate :performed_at_cannot_be_in_the_future
+
+  private
+
+  def performed_at_cannot_be_in_the_future
+    return if performed_at.blank?
+
+    if performed_at > Time.current
+      errors.add(:performed_at, "cannot be in the future")
+    end
+  end
 end
